96c7ecd90848c3773d04138999e8ddc3b4644fd4
OmikM <Omik.mueller@gmail.com>
2022-06-12 14:43:54 +0200

    bar animation

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from matplotlib import pyplot as plt
+from ast import While
 import pandas as pd
+from matplotlib import pyplot as plt
 df = pd.DataFrame({"inflacjaPL": [0,-0.9,-0.7,2,1.8,2.2,3.4],
                    "inflacjaUSA": [1.6,0.1,1.3,2.1,2.4,1.8,1.2],
                  "lata":[2014,2015,2016,2017,2018,2019,2020]})
 fig, ax = plt.subplots()
 lata = [2014,2015,2016,2017,2018,2019,2020]
-pl = plt.plot(lata,inflacjaPL,label="inflacja w polsce",marker='o')
-us = plt.plot(lata,inflacjaUSA,label="inflacja w U.S",linestyle = 'dotted',marker='o')
+plt.subplot(2, 1, 1)
+pl = plt.plot(lata,df.inflacjaPL,label="inflacja w polsce",marker='o')
+us = plt.plot(lata,df.inflacjaUSA,label="inflacja w U.S",linestyle = 'dotted',marker='o')
 plt.legend()
 plt.xlabel("rok")
 plt.ylabel("inflacja")
@@ -15,9 +17,17 @@ plt.grid(axis = 'y')
 
 for i in range(5):
     plt.plot(lata,
-            inflacjaPL,
+            df.inflacjaPL,
             marker='o',
             linewidth=2+(5*i),
             alpha=0.03,
             color='#00ff41')
+ax = plt.subplot(2, 1, 2)
+while plt.get_fignums():
+    i+=1
+    bars = plt.bar([0,1],[df.inflacjaPL[i%6],df.inflacjaUSA[i%6]], label=lata[i%6])
+    plt.legend()
+    plt.pause(1)
+    ax.clear()
+
 plt.show()
