2f7540b4d9b763f1dad41b841a9d903d1921e545
OmikM <Omik.mueller@gmail.com>
2022-06-30 00:06:52 +0200

    v3 bar animation update

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
-Program, wich shows inflation in Poland and U.S 2014-2022. program output is plot.
- Next I want to add more GUI fetures.I'm doing this project to get to know maplotlib. 
-first version:
+Program, that shows inflation in Poland and U.S 2014-2022. program output is plot. I'm doing this project to get to know maplotlib. 
+
+
+At this moment it looks like this v.3 (second plot is animated):
+
+![Figure_3](https://user-images.githubusercontent.com/93057360/176552983-4b608a8f-b279-4ee1-8011-754b10a66bb7.png)
+
+v.2:
+
+![download-1](https://user-images.githubusercontent.com/93057360/171998473-d101f588-e7a4-488d-a572-a29f2e63a12f.png)
+
+v.1:
 
 ![download](https://user-images.githubusercontent.com/93057360/171951381-10697234-f231-4862-a9cb-2222bd8744ba.png)
 
-At this moment it looks like this:
-
-![download-1](https://user-images.githubusercontent.com/93057360/171998473-d101f588-e7a4-488d-a572-a29f2e63a12f.png)
--- main.py
+++ main.py
@@ -6,28 +6,31 @@ df = pd.DataFrame({"inflacjaPL": [0,-0.9,-0.7,2,1.8,2.2,3.4],
                  "lata":[2014,2015,2016,2017,2018,2019,2020]})
 fig, ax = plt.subplots()
 lata = [2014,2015,2016,2017,2018,2019,2020]
+plt.style.use('dark_background')
 plt.subplot(2, 1, 1)
 pl = plt.plot(lata,df.inflacjaPL,label="inflacja w polsce",marker='o')
 us = plt.plot(lata,df.inflacjaUSA,label="inflacja w U.S",linestyle = 'dotted',marker='o')
+
 plt.legend()
 plt.xlabel("rok")
 plt.ylabel("inflacja")
-plt.style.use("dark_background")
 plt.grid(axis = 'y')
 
+fig.patch.set_facecolor('black')
+
 for i in range(5):
-    plt.plot(lata,
-            df.inflacjaPL,
-            marker='o',
-            linewidth=2+(5*i),
-            alpha=0.03,
-            color='#00ff41')
+    plt.plot(lata,df.inflacjaPL,marker='o',linewidth=2+(5*i),alpha=0.03,color='#00ff41')
+
 ax = plt.subplot(2, 1, 2)
+plt.plot()
 while plt.get_fignums():
     i+=1
-    bars = plt.bar([0,1],[df.inflacjaPL[i%6],df.inflacjaUSA[i%6]], label=lata[i%6])
-    plt.legend()
-    plt.pause(1)
+    plt.ylim(-1,4)
+    plt.bar(["Polska","USA"],[df.inflacjaPL[i%6],df.inflacjaUSA[i%6]], label=[lata[i%6]], color=['#80ff9f','#ffff99'])
+    plt.legend(loc="upper right")
+    plt.xlabel("kraj")
+    plt.ylabel("inflacja")
+    plt.grid(axis="y")
+    plt.pause(2)
     ax.clear()
-
 plt.show()
